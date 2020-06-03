e4b7e2b5c34a4b1f4b71bbd32f87cc6ec5840fa1
malik <assassinate75@gmail.com>
2020-06-03 14:20:15 +0700

    adjust config database

--- application/config/database.php
+++ application/config/database.php
@@ -75,9 +75,9 @@ $query_builder = TRUE;
 
 $db['default'] = array(
     'dsn'	=> '',
-    'hostname' => 'localhost',
+    'hostname' => 'db',
     'username' => 'root',
-    'password' => '',
+    'password' => 'adminlocal',
     'database' => 'modesy',
     'dbdriver' => 'mysqli',
     'dbprefix' => '',
